5bcc1a059c56c4a26697d9bd681a2bfe3777990d
mateceb <matekcebrat@gmail.com>
2017-12-18 22:43:32 +0100

    cz_1

---
+++ app/src/main/java/com/example/lenovo/laboratoriumpum3/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.lenovo.laboratoriumpum3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    Bundle bundle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
