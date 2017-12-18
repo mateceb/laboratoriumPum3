e7a17b543412885909110c1a1cb427fd48797279
mateceb <matekcebrat@gmail.com>
2017-12-18 22:54:41 +0100

    Zad3

--- app/src/main/java/com/example/lenovo/laboratoriumpum3/Main2Activity.java
+++ app/src/main/java/com/example/lenovo/laboratoriumpum3/Main2Activity.java
@@ -3,13 +3,15 @@ package com.example.lenovo.laboratoriumpum3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
+        ButterKnife.bind(this);
 
     }
 }
--- app/src/main/java/com/example/lenovo/laboratoriumpum3/MainActivity.java
+++ app/src/main/java/com/example/lenovo/laboratoriumpum3/MainActivity.java
@@ -1,15 +1,55 @@
 package com.example.lenovo.laboratoriumpum3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button_change_place)
+    Button button_change_place;
+
+    @BindView(R.id.button_change_activity)
+    Button button_change_activity;
+
+    @BindView(R.id.city)
+    TextView cityTv;
+
+    @BindView(R.id.insert_name)
+    EditText insert_name_city;
+
+    @OnClick(R.id.button_change_place)
+    void onClick()
+    {
+
+        cityTv.setText(insert_name_city.getText().toString());
+
+    }
+
+    @OnClick(R.id.button_change_activity)
+    void onClick1()
+    {
+        Intent intent = new Intent(this, Main2Activity.class );
+        startActivity(intent);
+
+    }
+
     Bundle bundle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
+        bundle = new Bundle();
     }
 
 
